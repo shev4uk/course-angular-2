eca24d564e78f6052f571c45bc3bc94cc6c17b59
shev4uk <shev4ukyuri@gmail.com>
2019-09-17 20:00:50 +0300

    type data

--- lessons/src/app/app.component.ts
+++ lessons/src/app/app.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+type User = {
+  name: string,
+  age: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +12,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lessons';
+
+  name: string = 'Yurii';
+  number = 10;
+
+  some: any = 1;
+
+  arrString: string[] = ['html', 'css', 'js', 'angular'];
+  users: User[] = [{
+    name: 'Bob',
+    age: 20
+  }, {
+    name: 'Alice',
+    age: 21
+  }];
+
+
 }
