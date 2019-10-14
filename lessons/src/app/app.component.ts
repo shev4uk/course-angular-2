29949d8df660b067001cc3c3230ba29ccd6974f2
shev4uk <shev4ukyuri@gmail.com>
2019-10-14 22:33:21 +0300

    auth

--- lessons/src/app/app.component.ts
+++ lessons/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,58 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lessons';
+
+  url = 'http://localhost:8000';
+  errors;
+
+  constructor(
+    private http: HttpClient
+  ) {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+  handleErrors(res) {
+    switch(res.status) {
+      case 401:
+        this.errors = res.message;
+    }
+  }
+
+  loginUser() {
+    this.http.post(`${this.url}/auth/login`, {"email":"bruno@email.com","password":"bruno"})
+      .pipe(
+        map( data => data ),
+        catchError (
+          er => {
+            console.log(er);
+            this.handleErrors(er);
+            return throwError(er);
+          }
+        )
+      )
+      .subscribe((data) => {
+        console.log(data);
+      })
+  }
+
+  regUser() {
+    this.http.post(`${this.url}/auth/register`, {"email":"bruno1@email.com","password":"bruno"})
+      .pipe(
+        map( data => data ),
+        catchError (
+          er => {
+            console.log(er);
+            this.handleErrors(er);
+            return throwError(er);
+          }
+        )
+      )
+      .subscribe((data) => {
+        console.log(data);
+      })
+  }
 }
