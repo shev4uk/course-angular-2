5abee9059cc9369838c7bbd09a0499c47406fda3
shev4uk <shev4ukyuri@gmail.com>
2019-10-16 23:14:21 +0300

    template

--- lessons/src/app/app.component.ts
+++ lessons/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map, catchError } from 'rxjs/operators';
-import { throwError, Observable } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -10,84 +8,10 @@ import { throwError, Observable } from 'rxjs';
 })
 export class AppComponent {
 
-  // intercepter
-  // https://stackoverflow.com/questions/47400929/how-to-add-authorization-header-to-angular-http-request
-
-  url = 'http://localhost:8000';
-  errors;
-
-  constructor(
-    private http: HttpClient
-  ) {
-
-  }
+  constructor() {}
 
   ngOnInit() {
-    const reqHeader = new HttpHeaders({ 
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + localStorage.getItem('token')
-    });
-    this.http.get(`${this.url}/events`, { headers: reqHeader }).subscribe( res => {
-      console.log(res);
-    })
-    // filter
-    this.http.get(`${this.url}/events?categoryId=1&categoryId=2`, { headers: reqHeader }).subscribe( res => {
-      console.log(res);
-    })
-    // sort
-    this.http.get(`${this.url}/events?_sort=price&_order=asc`, { headers: reqHeader }).subscribe( res => {
-      console.log(res);
-    })
-    // range
-    this.http.get(`${this.url}/events?price_gte=300&price_lte=500`, { headers: reqHeader }).subscribe( res => {
-      console.log(res);
-    })
-    // search
-    this.http.get(`${this.url}/events?q=internet`, { headers: reqHeader }).subscribe( res => {
-      console.log(res);
-    })
+    console.log(environment.url);
   }
 
-  handleErrors(res) {
-    switch(res.status) {
-      case 401:
-        this.errors = res.message;
-    }
-  }
-
-  loginUser() {
-    this.http.post(`${this.url}/auth/login`, {"email":"bruno@email.com","password":"bruno"})
-      .pipe(
-        map( data => data ),
-        catchError (
-          er => {
-            console.log(er);
-            this.handleErrors(er);
-            return throwError(er);
-          }
-        )
-      )
-      .subscribe((data) => {
-        localStorage.setItem('token', data['access_token']);
-        console.log(data);
-      })
-  }
-
-  regUser() {
-    this.http.post(`${this.url}/auth/register`, {"email":"bruno1@email.com","password":"bruno"})
-      .pipe(
-        map( data => data ),
-        catchError (
-          er => {
-            console.log(er);
-            this.handleErrors(er);
-            return throwError(er);
-          }
-        )
-      )
-      .subscribe((data) => {
-        localStorage.setItem('token', data['access_token']);
-        console.log(data);
-      })
-  }
 }
