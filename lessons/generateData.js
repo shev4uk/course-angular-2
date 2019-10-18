92d9ea48dfac961ed676ee4c5abb311e5f323185
shev4uk <shev4ukyuri@gmail.com>
2019-10-18 20:31:13 +0300

    draft

---
+++ lessons/generateData.js
@@ -0,0 +1,54 @@
+var faker = require('faker');
+
+var database = {
+  events: [],
+  categories: [],
+  wishlist: []
+};
+
+var users = {
+  users: []
+}
+
+var quantityEvent = 10;
+
+for (var i = 1; i <= quantityEvent; i++) {
+  database.events.push({
+    id: i,
+    name: faker.random.words(),
+    image: faker.image.technics(),
+    date: faker.date.between('2015-01-01', '2015-12-31'),
+    city: faker.address.city(),
+    location: {
+      latitude: faker.address.latitude(),
+      longitude: faker.address.longitude()
+    },
+    desc: faker.lorem.text(),
+    categoryId: faker.random.number({ min: 1, max: 10 }),
+    price: faker.random.number({ min: 250, max: 1000 })
+  });
+}
+for (var i = 1; i <= 10; i++) {
+  database.categories.push({
+    id: i,
+    name: faker.name.jobType(),
+  });
+}
+for (var i = 1; i <= 10; i++) {
+  database.wishlist.push({
+    id: i,
+    eventId: faker.random.number({ min: 1, max: quantityEvent }),
+  });
+}
+
+// for (var i = 1; i <= 10; i++) {
+//   users.users.push({
+//     id: i,
+//     name: faker.name.findName(),
+//     email: faker.internet.email(),
+//     password: faker.internet.email()
+//   });
+// }
+// console.log(JSON.stringify(users));
+
+console.log(JSON.stringify(database));
